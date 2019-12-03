87077c0b6624806ecb4371f9f76e92fd36a14f17
Woody <woody@gmail.com>
2019-12-03 09:04:10 +0700

    Init flutter plugin.

---
+++ android/src/main/kotlin/com/bh/flashlight/flashlight/FlashlightPlugin.kt
@@ -0,0 +1,25 @@
+package com.bh.flashlight.flashlight
+
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+class FlashlightPlugin: MethodCallHandler {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "flashlight")
+      channel.setMethodCallHandler(FlashlightPlugin())
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+}
