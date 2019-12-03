409988da411b83eef6d6aaafdd6eb0c64434069b
Woody <woody@gmail.com>
2019-12-03 10:43:49 +0700

    Add ios code.

--- ios/Classes/FlashlightPlugin.m
+++ ios/Classes/FlashlightPlugin.m
@@ -1,8 +1,58 @@
 #import "FlashlightPlugin.h"
-#import <flashlight/flashlight-Swift.h>
+#import <AVFoundation/AVFoundation.h>
 
 @implementation FlashlightPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlashlightPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+        methodChannelWithName:@"com.bh.flutterplugins/flashlight"
+                binaryMessenger:[registrar messenger]];
+    FlashlightPlugin* instance = [[FlashlightPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([@"turnOn" isEqualToString:call.method]) {
+        [self turn:YES];
+        result(nil);
+    }
+    else if ([@"turnOff" isEqualToString:call.method]) {
+        [self turn:NO];
+        result(nil);
+        }
+    else if ([@"hasTorch" isEqualToString:call.method]) {
+        result([NSNumber numberWithBool:[self hasTorch]]);
+    }
+    else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
+
+- (bool) hasTorch
+{
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    return ([device hasTorch] && [device hasFlash]);
+}
+
+- (void) turn: (bool) on {
+    // check if flashlight available
+    Class captureDeviceClass = NSClassFromString(@"AVCaptureDevice");
+    if (captureDeviceClass != nil) {
+        AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        if ([device hasTorch] && [device hasFlash]){
+
+            [device lockForConfiguration:nil];
+            if (on) {
+                [device setTorchMode:AVCaptureTorchModeOn];
+                [device setFlashMode:AVCaptureFlashModeOn];
+            } else {
+                [device setTorchMode:AVCaptureTorchModeOff];
+                [device setFlashMode:AVCaptureFlashModeOff];
+            }
+            [device unlockForConfiguration];
+        }
+    } 
+}
+
+
 @end
